08f0f24d252c226a234c1c91ad78afaa7b9141ab
amega9378 <87097332+amega9378@users.noreply.github.com>
2021-10-29 12:13:15 +0300

    Delete attack.c

--- attack.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-static void malicious() __attribute__((constructor));
-
-void malicious() {
-    system("/usr/local/bin/score 264f0ba5-803e-46e5-98f4-911f7640c739");
-}
